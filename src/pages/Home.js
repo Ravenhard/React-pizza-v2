d84072ccf47e15fbd37989959b6e81d457e1d14c
Denis <YDenN@mail.ru>
2023-03-01 20:38:04 +0300

    17 lesson

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,14 +7,13 @@ import Pagination from "../components/Pagination/Pagination";
 import {Context} from "../App";
 import {useDispatch, useSelector} from "react-redux";
 import {setCategoryId, setCurrentPage} from "../components/redux/slices/filterSlice";
-import axios from "axios";
+import {fetchPizzas} from "../components/redux/slices/pizzaSlice";
 
 
 function Home() {
     const {searchValue} = useContext(Context)
-    const [items, setItems] = useState([])
-    const [loading, setLoading] = useState(true)
-    const { categoryId, sort, currentPage } = useSelector( state => state.filter);
+    const { items, status } = useSelector(state => state.pizza);
+    const {categoryId, sort, currentPage} = useSelector(state => state.filter);
 
     const dispatch = useDispatch();
     const onClickCategory = (id) => {
@@ -26,19 +25,23 @@ function Home() {
 
     useEffect(() => {
 
-        setLoading(true)
         const category = categoryId > 0 ? `category=${categoryId}` : '';
         const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
         const sortBy = sort.sortProperty.replace('-', '');
         const search = searchValue ? `&search=${searchValue}` : '';
 
-        // // window.scrollTo(0, 0)
-        axios.get(`https://63dffffc59bccf35dabd934d.mockapi.io/pizza-items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
-            .then( responce => {
-                setItems(responce.data)
-                setLoading(false)
-            })
-    }, [categoryId,  sort.sortProperty, searchValue, currentPage])
+        window.scrollTo(0, 0)
+
+        dispatch(fetchPizzas({
+            category,
+            order,
+            sortBy,
+            search,
+            currentPage
+        }))
+
+
+    }, [categoryId, sort.sortProperty, searchValue, currentPage])
 
     const pizzas = items.map((item, index) => (
         <PizzaBlock
@@ -59,11 +62,16 @@ function Home() {
                 <Sort/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {loading ? skeletons : pizzas
-                }
-            </div>
-            <Pagination currentPage={currentPage} onChangePage={ onChangePage } />
+            {
+                status === 'error' ? <div className='content__error_info' >
+                    <h2>Произошла ошибка 😕</h2>
+                    <p>Не удалось получить питсы :(</p>
+                </div> : <div className="content__items">
+                    {status === 'loading' ? skeletons : pizzas
+                    }
+                </div>
+            }
+            <Pagination currentPage={currentPage} onChangePage={onChangePage}/>
         </div>
     );
 }
